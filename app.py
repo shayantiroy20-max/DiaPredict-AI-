d32cd90a0ec21cb8a32b490dd4aaa4a278364db8
N Biraja Isac <nbiraja.isacfcs@kiit.ac.in>
2026-05-12 15:52:52 +0530

    Fixed Render deployment

--- app.py
+++ app.py
@@ -420,7 +420,14 @@ def dashboard():
 def logout():
     return redirect(url_for("home"))
 if __name__ == "__main__":
+    import os
+    port = int(
+        os.environ.get(
+            "PORT",
+            5000
+        )
+    )
     app.run(
-        debug=True,
-        port=5001
+        host="0.0.0.0",
+        port=port
     )
